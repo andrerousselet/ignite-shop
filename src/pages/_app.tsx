1f473e8f732bb52c5eb95cd2c3cfe61304865725
Andre Rousselet <andre098@hotmail.com>
2023-07-21 16:13:51 -0300

    feat(header-and-home): add header and home page styling with images and logo

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,17 @@
 import { globalStyles } from "../styles/global";
+import logoImg from "../assets/logo.svg";
+import { Container, Header } from "../styles/pages/app";
+import Image from "next/image";
 
 globalStyles();
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <Container>
+      <Header>
+        <Image src={logoImg} alt="" />
+      </Header>
+      <Component {...pageProps} />
+    </Container>
+  );
 }
