a78968205605e5065ee75be13f2c18df4ef6f4b0
Andre Rousselet <andre098@hotmail.com>
2023-07-24 16:10:35 -0300

    feat(_app): add handbag container with styling

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,13 @@
 import { globalStyles } from "../styles/global";
 import logoImg from "../assets/logo.svg";
-import { Container, Header } from "../styles/pages/app";
+import {
+  CartQty,
+  Container,
+  HandbagContainer,
+  Header,
+} from "../styles/pages/app";
 import Image from "next/image";
+import { Handbag } from "@phosphor-icons/react";
 
 globalStyles();
 
@@ -10,6 +16,12 @@ export default function App({ Component, pageProps }) {
     <Container>
       <Header>
         <Image src={logoImg} alt="" />
+        <HandbagContainer>
+          <Handbag size={24} color="#8D8D99" />
+          <CartQty>
+            <span>1</span>
+          </CartQty>
+        </HandbagContainer>
       </Header>
       <Component {...pageProps} />
     </Container>
--- src/styles/pages/app.ts
+++ src/styles/pages/app.ts
@@ -13,4 +13,38 @@ export const Header = styled("header", {
   width: "100%",
   maxWidth: 1180,
   margin: "0 auto",
+
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "space-between",
+});
+
+export const HandbagContainer = styled("button", {
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+  padding: "0.75rem",
+  backgroundColor: "$gray800",
+  position: "relative",
+});
+
+export const CartQty = styled("div", {
+  position: "absolute",
+  top: -10,
+  right: -10,
+  border: "3px solid #121214",
+  borderRadius: "100%",
+  width: 27,
+  height: 27,
+  backgroundColor: "$green500",
+
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+
+  span: {
+    color: "$white",
+    fontSize: 14,
+    fontWeight: "bold",
+  },
 });
